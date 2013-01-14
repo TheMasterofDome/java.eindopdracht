d87e9fa5acd38a7452af9195a9e156da022c1fd8
Irene <Irene@Irene-PC.home>
2013-01-14 17:43:26 +0100

    Ik heb een klasse Boswachter.java aangemaakt

---
+++ Eindopdracht/src/bosbrand/model/Boswachter.java
@@ -0,0 +1,43 @@
+package bosbrand.model;
+
+public class Boswachter implements IBoswachter{
+
+	
+		/**
+		 * de rij waarop deze boswachter staat
+		 * @return de rij waarop deze boswachter staat
+		 */
+		public int getRij(){
+			return 0;
+		}
+		/**
+		 * de kolom waarop deze boswachter staat
+		 * @return de kolom waarop deze boswachter staat
+		 */
+		public int getKolom(){
+			return 0;
+		}
+		
+		/**
+		 * doof alle branden om de boswachter heen, en laat hem een actie uitvoeren om effectief
+		 * volgende branden te bestrijden.
+		 */
+		public void update(){
+		}
+		
+		/**
+		 * stuur een bericht aan alle boswachters. U hoeft dit niet te implementeren, maar het kan
+		 * handig zijn als U voor de bonus wilt gaan.
+		 * @param het bericht dat verstuurd wordt
+		 */
+		public void stuurBericht(String bericht){
+		}
+		/**
+		 * haal alle berichten op die zijn verstuurd sinds de laatste keer dat deze actie werd
+		 * uitgevoerd. U hoeft dit niet te implementeren, maar het kan handig zijn als U voor de
+		 * bonus wilt gaan.
+		 */
+		public void ontvangBerichten(){
+		}
+	
+}
