b858427ea9c97864cfee97672d19ac8d7981d288
TheMasterofDome <themasterofdome@gmail.com>
2013-01-14 18:23:42 +0100

    Update Eindopdracht/src/bosbrand/model/Boom.java

--- Eindopdracht/src/bosbrand/model/Boom.java
+++ Eindopdracht/src/bosbrand/model/Boom.java
@@ -1,4 +1,5 @@
 package bosbrand.model;
+//HOI
 public abstract class Boom implements IBoom {
 	// twee constanten om inbrandheid uit te drukken. Het keyword 'final' zegt dat
 	// deze waarden niet veranderd kunnen worden tijdens het draaien van het
