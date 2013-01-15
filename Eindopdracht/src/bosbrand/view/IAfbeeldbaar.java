b9afea20104d12669159e7368b1f352a1144e9fb
Thomas <themasterofdome@gmail.com>
2013-01-15 12:25:31 +0100

    IAfbeeldbaar klaar.

--- Eindopdracht/src/bosbrand/view/IAfbeeldbaar.java
+++ Eindopdracht/src/bosbrand/view/IAfbeeldbaar.java
@@ -6,4 +6,35 @@ package bosbrand.view;
  */
 public interface IAfbeeldbaar {
 
+	/**
+	 * Deze methode geeft de kleur van het af te beelden object.
+	 */
+	public void getColor();
+	
+	/**
+	 * Deze methode geeft de af te drukken letter.
+	 */
+	public char getLetter();
+	
+	/**
+	 * Deze methode geeft de lijndikte van het af te beelden vierkant.
+	 */
+	public int getLijndikte();
+	
+	/**
+	 * Deze methode geeft true desda er een boswachter op dit kavel staat.
+	 */
+	public boolean isThereBoswachter();
+	
+	/**
+	 * Deze methode geeft de x-coordinaat van het af te beelden vlak.
+	 * @param rij de rij waarin het kavel zich bevindt.
+	 */
+	public int getX(int rij);
+	
+	/**
+	 * Deze methode geeft de y-coordinaat van het af te beelden vlak.
+	 * @param kolom de kolom waarin het kavel zich bevindt.
+	 */
+	public int getY(int kolom);
 }
