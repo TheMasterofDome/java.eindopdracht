06589b64a0f8e6ce3ba2ba63b0ee68c59444432f
Irenesirene <irenerademaker92@gmail.com>
2013-01-18 13:26:12 +0100

    Als toggleVuur wordt aangeroepen terwijl er niet wordt gesimuleerd, wordt het beeld nu ook geupdatet.

--- Eindopdracht/src/bosbrand/view/View.java
+++ Eindopdracht/src/bosbrand/view/View.java
@@ -14,9 +14,11 @@ public class View implements IView, ComponentListener {
 	private IController controller;
 
 	/**
-	 * Constructor die het frame en de controls erop aanmaakt.
-	 * Ook stelt hij het midden in en initialiseert hij de rest van de variabelen.
-	 * @param controller die deze view en het bijbehorende model bestuurt
+	 * Constructor die het frame en de controls erop aanmaakt. Ook stelt hij het
+	 * midden in en initialiseert hij de rest van de variabelen.
+	 * 
+	 * @param controller
+	 *            die deze view en het bijbehorende model bestuurt
 	 */
 	public View(IController controller) {
 		moving = false;
@@ -36,7 +38,9 @@ public class View implements IView, ComponentListener {
 
 	/**
 	 * middelpunt van het venster.
-	 * @return een java.awt.Point object dat het midden van het af te beelden gebied weergeeft.
+	 * 
+	 * @return een java.awt.Point object dat het midden van het af te beelden
+	 *         gebied weergeeft.
 	 */
 	public Point getMidden() {
 		return midden;
@@ -46,7 +50,9 @@ public class View implements IView, ComponentListener {
 	 * De breedte is de breedte van het venster minus 102 pixels, wat in de
 	 * praktijk de breedte van de Box met JButtons is, gedeeld door twee. De
 	 * hoogte s de hoogte van het venster gedeeld door twee.
-	 * @param een java.awt.Point.
+	 * 
+	 * @param een
+	 *            java.awt.Point.
 	 */
 	public void setMidden(Point midden) {
 		int width = (1024 - 102) / 2;
@@ -57,13 +63,16 @@ public class View implements IView, ComponentListener {
 	/**
 	 * afbeelden van de meegegeven data, met het 'midden' punt in het midden van
 	 * het veld en met een hoogte en breedte die leegmaken afhangen van 'zoom'
-	 * @param afTeBeeldenData een array met afbeeldbare data
+	 * 
+	 * @param afTeBeeldenData
+	 *            een array met afbeeldbare data
 	 */
 	public void afbeelden(IAfbeeldbaar[] afTeBeeldenData) {
 		// Venster leegmaken
 		dpTekenGebied.leegmaken();
 
-		// Loop door de array en beeld elk kavel af met behulp van zijn Afbeeldbaar object.
+		// Loop door de array en beeld elk kavel af met behulp van zijn
+		// Afbeeldbaar object.
 		for (IAfbeeldbaar beelden : afTeBeeldenData) {
 			dpTekenGebied.tekenRechthoek(beelden.getX(), beelden.getY(),
 					beelden.getZijde(), beelden.getZijde(), beelden.getColor());
@@ -79,7 +88,8 @@ public class View implements IView, ComponentListener {
 	private void createUI() {
 		// maak een nieuw frame aan met de naam "Bosbrand"
 		jfMainFrame = new JFrame("Bosbrand");
-		// voeg deze klasse zelf toe als componentlistener, wordt alleen gebruikt voor resizen van het frame
+		// voeg deze klasse zelf toe als componentlistener, wordt alleen
+		// gebruikt voor resizen van het frame
 		jfMainFrame.addComponentListener(this);
 		// als het frame gesloten wordt, houdt het programma op met draaien
 		jfMainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -96,9 +106,12 @@ public class View implements IView, ComponentListener {
 		pButtonArea.setLayout(new BoxLayout(pButtonArea, BoxLayout.Y_AXIS));
 		content.add(pButtonArea, BorderLayout.LINE_END);
 
-		// maak de reset-knop en roep de methode doeReset() aan als op deze knop wordt gedrukt
-		// TO DO: zorgen dat "reset" doeReset aanroept en dat "Simuleer" doeSimuleer aanroept.
-		// nu: bij Simuleer gebeurt niks, bij Reset worden ze allebei aangeroepen.
+		// maak de reset-knop en roep de methode doeReset() aan als op deze knop
+		// wordt gedrukt
+		// TO DO: zorgen dat "reset" doeReset aanroept en dat "Simuleer"
+		// doeSimuleer aanroept.
+		// nu: bij Simuleer gebeurt niks, bij Reset worden ze allebei
+		// aangeroepen.
 		JButton btnGenerate = new JButton("Reset");
 		pButtonArea.add(btnGenerate);
 		ActionListener listenerGenerate = new ActionListener() {
@@ -108,7 +121,8 @@ public class View implements IView, ComponentListener {
 		};
 		btnGenerate.addActionListener(listenerGenerate);
 
-		// maak de simulate-knop en roep de methode doeSimuleer() aan als op deze knop wordt gedrukt.
+		// maak de simulate-knop en roep de methode doeSimuleer() aan als op
+		// deze knop wordt gedrukt.
 		JButton btnSimulate = new JButton("Simuleer");
 		pButtonArea.add(btnSimulate);
 		ActionListener listenerSimulate = new ActionListener() {
@@ -128,56 +142,43 @@ public class View implements IView, ComponentListener {
 			// methode aan. De methodes worden aangeroepen op rij en kolom,
 			// welke worden berekend adhv het coördinaat waarop geklikt wordt
 			public void mouseClicked(MouseEvent e) {
-				System.out.println("X-coördinaat muisklik: " + e.getX());
-				System.out.println("Y-coördinaat muisklik: " + e.getY());
 
-				System.out.println("X-coördinaat midden: " + midden.getX());
-				System.out.println("Y-coördinaat midden: " + midden.getY());
 				double kavelsInBreedte = ((Controller) controller)
 						.berekenAantalKavelsBreedte();
-				System.out.println("aantal kavels in breedte is "
-						+ kavelsInBreedte);
+
 				double kavelsInLengte = ((Controller) controller)
 						.berekenAantalKavelsLengte();
-				System.out.println("aantal kavels in lengte is "
-						+ kavelsInLengte);
 
 				double linkerGrensVenster = midden.getX()
 						- ((kavelsInBreedte / 2.0) * Afbeeldbaar.zijde);
-				System.out.println("kavels in breedte / 2: " + kavelsInBreedte
-						/ 2.0);
-				System.out.println("grens venster links is "
-						+ linkerGrensVenster);
+
 				double bovenGrensVenster = midden.getY()
 						- ((kavelsInLengte / 2.0) * Afbeeldbaar.zijde);
-				System.out.println("kavels in lengte / 2: " + kavelsInLengte
-						/ 2.0);
-				System.out.println("grens venster boven is "
-						+ bovenGrensVenster);
 
 				int kolom = (int) ((e.getX() - linkerGrensVenster) / Afbeeldbaar.zijde);
-				System.out.println();
 
-				System.out.println("kolom is " + kolom);
 				int rij = (int) ((e.getY() - bovenGrensVenster) / Afbeeldbaar.zijde);
 
-				System.out.println("rij is " + rij);
-
 				if (SwingUtilities.isLeftMouseButton(e)) {
-					System.out.println("X: " + e.getX() + "Y: " + e.getY());
+
 					controller.toggleBoswachter(rij, kolom);
+					controller.afbeelden();
 				} else if (SwingUtilities.isRightMouseButton(e)) {
 					controller.toggleVuur(rij, kolom);
+					controller.afbeelden();
 				}
 			}
-			
-			//Deze submethode stelt het coordinaat in waar de muis is als je bgint met slepen (klikken).
+
+			// Deze submethode stelt het coordinaat in waar de muis is als je
+			// bgint met slepen (klikken).
 			public void mousePressed(MouseEvent e) {
 				start_end_coords[2] = e.getX();
 				start_end_coords[3] = e.getY();
 			}
-			//Deze submethode stelt het coordinaat in waar de muis is als je ophoudt met slepen.
-			//Verplaats daarna direct het beeld.
+
+			// Deze submethode stelt het coordinaat in waar de muis is als je
+			// ophoudt met slepen.
+			// Verplaats daarna direct het beeld.
 			public void mouseReleased(MouseEvent e) {
 				if (moving) {
 					start_end_coords[0] = e.getX();
@@ -186,20 +187,27 @@ public class View implements IView, ComponentListener {
 					moving = false;
 				}
 			}
-			public void mouseEntered(MouseEvent e) {}
-			public void mouseExited(MouseEvent e) {}			
+
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			public void mouseExited(MouseEvent e) {
+			}
 		};
-		
+
 		dpTekenGebied.addMouseListener(muis);
-			
-		//Dit object en submethode zetten moving op true als de muis aan het slepen is.
+
+		// Dit object en submethode zetten moving op true als de muis aan het
+		// slepen is.
 		MouseMotionListener muisMove = new MouseMotionListener() {
 			public void mouseDragged(MouseEvent e) {
-				moving  = true;
+				moving = true;
 			}
-			public void mouseMoved(MouseEvent e) {}
+
+			public void mouseMoved(MouseEvent e) {
+			}
 		};
-		
+
 		dpTekenGebied.addMouseMotionListener(muisMove);
 	}
 
@@ -212,18 +220,22 @@ public class View implements IView, ComponentListener {
 	}
 
 	/**
-	 * De methode die het veld op de nieuwe positie afbeeldt.
-	 * Dit door de verplaatsing te berekenen, dit aan het startpunt te geven en opnieuw af te beelden.
-	 * @param i een array van integers die de coordinaten voorstellen startX, startY, eindX, eindY.
+	 * De methode die het veld op de nieuwe positie afbeeldt. Dit door de
+	 * verplaatsing te berekenen, dit aan het startpunt te geven en opnieuw af
+	 * te beelden.
+	 * 
+	 * @param i
+	 *            een array van integers die de coordinaten voorstellen startX,
+	 *            startY, eindX, eindY.
 	 */
 	private void berekenVerplaatsing(Integer[] i) {
 		int xVerschil = start_end_coords[2] - start_end_coords[0];
 		int yVerschil = start_end_coords[3] - start_end_coords[1];
-		
+
 		((Controller) controller).setStartpunt(xVerschil, yVerschil);
 		controller.afbeelden();
 	}
-	
+
 	/**
 	 * vertelt het tekenpanel om zijn resizecode te draaien, en laat daarna de
 	 * controller het beeld opnieuw afbeelden
@@ -237,7 +249,9 @@ public class View implements IView, ComponentListener {
 	 * zoom als een ratio ten opzichte van normaal (dus 1.0 is normaal, 2.0 is
 	 * twee keer zo groot of twee keer zo klein, afhankelijk van de
 	 * implementatie).
-	 * @param zoomniveau als ratio ten opzichte van normaal
+	 * 
+	 * @param zoomniveau
+	 *            als ratio ten opzichte van normaal
 	 */
 	public void setZoom(double zoom) {
 		// BONUS: voeg code toe om netjes te kunnen zoomen
@@ -246,7 +260,8 @@ public class View implements IView, ComponentListener {
 	/**
 	 * zoom als een ratio ten opzichte van normaal (dus 1.0 is normaal, 2.0 is
 	 * twee keer zo groot of twee keer zo klein, afhankelijk van de
-	 * implementatie). 
+	 * implementatie).
+	 * 
 	 * @return zoomniveau als ratio ten opzichte van normaal
 	 */
 	public double getZoom() {
@@ -256,6 +271,7 @@ public class View implements IView, ComponentListener {
 
 	/**
 	 * rotatie in radialen
+	 * 
 	 * @return double die de rotatie in radialen geeft ten opzichte van neutraal
 	 */
 	public double getRotatie() {
@@ -265,13 +281,20 @@ public class View implements IView, ComponentListener {
 
 	/**
 	 * geef rotatie in radialen
-	 * @param rotatie de rotatie in radialen ten opzichte van neutraal
+	 * 
+	 * @param rotatie
+	 *            de rotatie in radialen ten opzichte van neutraal
 	 */
 	public void setRotatie(double dRotatie) {
 		// BONUS: voeg code toe om netjes te kunnen roteren
 	}
-	
-	public void componentHidden(ComponentEvent e) {}
-	public void componentShown(ComponentEvent e) {}
-	public void componentMoved(ComponentEvent e) {}
+
+	public void componentHidden(ComponentEvent e) {
+	}
+
+	public void componentShown(ComponentEvent e) {
+	}
+
+	public void componentMoved(ComponentEvent e) {
+	}
 }
